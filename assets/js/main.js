35615ca326675bf4469d3ca1a2b7b93a47306fac
duyanwei <duyanwei0702@gmail.com>
2018-10-07 12:26:41 +0800

    updated color

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,29 +12,42 @@ var resetNav = function() {
 	nav4.style.background='';
 	nav5.style.background='';
 	nav6.style.background='';
+
+  nav1.style.color='#ffffff';
+	nav2.style.color='#ffffff';
+	nav3.style.color='#ffffff';
+	nav4.style.color='#ffffff';
+	nav5.style.color='#ffffff';
+	nav6.style.color='#ffffff';
 };
 
 nav1.addEventListener("click", function() {
 	resetNav();
-	nav1.style.background = 'gray';
+  nav1.style.background = 'white';
+	nav1.style.color = 'black';
 });
 nav2.addEventListener("click", function() {
 	resetNav();
-	nav2.style.background = 'gray';
+	nav2.style.background = 'white';
+	nav2.style.color = 'black';
 });
 nav3.addEventListener("click", function() {
 	resetNav();
-	nav3.style.background = 'gray';
+	nav3.style.background = 'white';
+	nav3.style.color = 'black';
 });
 nav4.addEventListener("click", function() {
 	resetNav();
-	nav4.style.background = 'gray';
+	nav4.style.background = 'white';
+	nav4.style.color = 'black';
 });
 nav5.addEventListener("click", function() {
 	resetNav();
-	nav5.style.background = 'gray';
+	nav5.style.background = 'white';
+	nav5.style.color = 'black';
 });
 nav6.addEventListener("click", function() {
 	resetNav();
-	nav6.style.background = 'gray';
+	nav6.style.background = 'white';
+	nav6.style.color = 'black';
 });
