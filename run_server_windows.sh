a895672030fe1f5b1af6ebd2f8566410211405e7
Yanwei Du <duyanwei0702@gmail.com>
2026-07-23 01:55:58 -0400

    Fix scholar address

--- run_server_windows.sh
+++ run_server_windows.sh
@@ -1,3 +1,6 @@
 # bundle exec jekyll liveserve
 
 bundle exec jekyll serve --host 0.0.0.0 --port 4000 --livereload
+
+# In your Windows browser, navigate to:http://<YOUR_WSL_IP>:4000/
+# Find your WSL IP: ip addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'
